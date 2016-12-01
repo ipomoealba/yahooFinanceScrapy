1e52852ab4fbcd5a5a5007f5bbe34bb31b825fbf
oscar <oscar84922tw@gmail.com>
2016-12-01 14:35:23 +0800

    add 南非幣 紐西蘭幣 in updatespider.py

--- yahooFinance/spiders/updateSpider.py
+++ yahooFinance/spiders/updateSpider.py
@@ -29,8 +29,10 @@ class updateSpider(scrapy.Spider):
     def parse_fund(self, response):
         row = response.xpath(
             '//tr[@class="Bgc-w alter-bg"]| //tr[@class="Bgc-w"]')
-        cur = [u'日圓',u'英鎊',u'新台幣',u'美元',u'港幣',u'韓元',u'加拿大幣',u'新加坡幣',u'人民幣',u'澳幣',u'印尼盾',u'泰銖',u'馬來西亞幣',u'菲律賓披索',u'歐元',u'越南盾']
-        cure = ['JPY','GBP','NTD','USD','HKD','KRW','CAD','SGD','CNY','AUD','IDR','THB','MYR','PHP','EUR','VND']
+        cur = [u'日圓', u'英鎊', u'新台幣', u'美元', u'港幣', u'韓元', u'加拿大幣', u'新加坡幣', u'人民幣',
+               u'澳幣', u'印尼盾', u'泰銖', u'馬來西亞幣', u'菲律賓披索', u'歐元', u'越南盾', u'南非幣', u'紐西蘭幣']
+        cure = ['JPY', 'GBP', 'NTD', 'USD', 'HKD', 'KRW', 'CAD', 'SGD', 'CNY',
+                'AUD', 'IDR', 'THB', 'MYR', 'PHP', 'EUR', 'VND', 'ZAR', 'NZD']
         for r in row:
             item = YahoofinanceItem()
             print(
@@ -45,13 +47,13 @@ class updateSpider(scrapy.Spider):
             # item['net_worth'] = (r.xpath('.//td[@class="Ell Ta-c closeprice"]/text()').extract()) \
             # .replace(" ","").replace( "\n", "")
             curC = r.xpath(
-                './/td[@class="Ell Ta-c"]/text()').extract()[0].replace(" ","").replace("\n","")
-            for i in range(0,len(cur)):
+                './/td[@class="Ell Ta-c"]/text()').extract()[0].replace(" ", "").replace("\n", "")
+            for i in range(0, len(cur)):
                 # print cur[i]
-		# print item['currency']
-		if curC == cur[i]:
-	            print curC + " " + cure[i]
-		    item['currency'] = cure[i]
-		#else:
-		  #  item['currency'] = "GG"
+                # print item['currency']
+                if curC == cur[i]:
+                    print curC + " " + cure[i]
+                    item['currency'] = cure[i]
+                # else:
+                  #  item['currency'] = "GG"
             yield item
